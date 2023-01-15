39817d932e9c05bbc05f5d75f52c6326e4eb047f
MD SALIM <mdsalim.iiitr@gmail.com>
2023-01-15 20:07:21 +0530

    Updates

---
+++ precache-manifest.647d68624628fed74c32125e66633221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fed16004c1a898a7318",
+    "url": "/TextUtils/static/css/main.9a96f18b.chunk.css"
+  },
+  {
+    "revision": "9fed16004c1a898a7318",
+    "url": "/TextUtils/static/js/main.9fed1600.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "16b4abf50e8c0b8dfb33bc89fdc49fb9",
+    "url": "/TextUtils/index.html"
+  }
+];
